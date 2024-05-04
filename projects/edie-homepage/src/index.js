0293165bf88886f47273789e16b89f185c7c9b02
Brando Vidal <yisusdracon@gmail.com>
2024-05-03 23:15:28 -0500

    feat: add edit homepage project

---
+++ projects/edie-homepage/src/index.js
@@ -0,0 +1,16 @@
+;(function () {
+  document.addEventListener('DOMContentLoaded', () => getMenu())
+})()
+
+function getMenu() {
+  const nav = document.querySelector('.container .nav')
+  if (!nav) return
+
+  const menu = nav.querySelector('#menu')
+  const navLinks = nav.querySelectorAll('.nav-items li > a')
+
+  // Change checked menu
+  menu &&
+    navLinks.length > 0 &&
+    navLinks.forEach((link) => link.addEventListener('click', () => (menu.checked = false)))
+}
